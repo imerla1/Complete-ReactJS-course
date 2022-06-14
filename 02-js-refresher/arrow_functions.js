dfefb2479d8b049c79d266a5eb5cb7ea94fff4e5
imerla1 <giorgi1048@yahoo.com>
2022-06-14 11:53:01 +0400

    Arrow Functions refresher

---
+++ 02-js-refresher/arrow_functions.js
@@ -0,0 +1,19 @@
+// normal Js function
+function printMyName(name) {
+    // function body
+    console.log(name)
+}
+
+// Arrow Function
+const printMyNameArr = (name) => {
+    // arrow fn body
+    console.log(name)
+}
+
+printMyName("george")
+printMyNameArr("George Imerlishvili")
+
+
+// takes a number as a parameter and Multiplies it by 2
+// Arr function
+const multiply = number => number * 2;
