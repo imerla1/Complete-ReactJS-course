1b41c30da30fdb7a42fb2e47d0736bf7a7195e86
imerla1 <giorgi1048@yahoo.com>
2022-06-22 12:41:38 +0400

    Moved React App from JsRefresher to it's own dir

---
+++ 03-react-basics/my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's Get started!</h2>
+      <p>This Is paragraph!</p>
+    </div>
+  )
+}
+export default App;
